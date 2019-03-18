88d8c7dad655012577deec54a2c9603269225de7
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-18 21:02:31 +0400

    Added Utils class for helper functions

---
+++ Neural Network/Main.cpp
@@ -0,0 +1,7 @@
+#include "Core.h"
+
+
+int main() {
+
+	return 0;
+}
---
+++ Neural Network/Utils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Utils
+{
+public:
+	static double randomNumber();
+	static double randomNumber(double from, double to);
+};
+
+double Utils::randomNumber() {
+	return 12.0; // TODO: to be implemented
+}
+
+double Utils::randomNumber(double from, double to) {
+	return 12.0; // TODO: to be implemented
+}
