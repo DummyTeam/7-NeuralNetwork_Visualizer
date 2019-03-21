f13899990de6016751f42124b5826eb09bb81cdf
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-21 23:45:46 +0400

    Updated Utils.h class

--- Neural Network/Utils.h
+++ Neural Network/Utils.h
@@ -3,14 +3,13 @@
 class Utils
 {
 public:
-	static double randomNumber();
-	static double randomNumber(double from, double to);
+	static double randomNumber()
+	{
+		return 12.0; // TODO: to be implemented
+	}
+
+	static double randomNumber(double from, double to)
+	{
+		return 12.0; // TODO: to be implemented
+	}
 };
-
-double Utils::randomNumber() {
-	return 12.0; // TODO: to be implemented
-}
-
-double Utils::randomNumber(double from, double to) {
-	return 12.0; // TODO: to be implemented
-}
