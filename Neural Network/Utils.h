a45dba55759837b25b8755e5a8939ba352acbcec
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-22 23:36:42 +0400

    Fixed randomNumber in Utils class

--- Neural Network/Utils.h
+++ Neural Network/Utils.h
@@ -1,15 +1,16 @@
 #pragma once
-
+#include"Core.h"
 class Utils
 {
 public:
+	// from 0 to 99
 	static double randomNumber()
 	{
-		return 12.0; // TODO: to be implemented
+		return (rand() % 10000) / 100.0;
 	}
 
 	static double randomNumber(double from, double to)
 	{
-		return 12.0; // TODO: to be implemented
+		return 	(rand() % ((int)(to - from) * 100)) / 100.0 + from;
 	}
 };
