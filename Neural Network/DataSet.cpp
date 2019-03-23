eb4d3bcdb8d7679a9b8cb5080f3de1a57aaf5bd0
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-23 23:11:39 +0400

    Created DataSet.cpp

---
+++ Neural Network/DataSet.cpp
@@ -0,0 +1,27 @@
+#include"DataSet.h"
+#include <fstream>
+
+DataSet::DataSet(std::string path, int sizeOfInput, int sizeOfOutput)
+{
+	this->path = path;
+	this->sizeOfInput = sizeOfInput;
+	this->sizeOfOutput = sizeOfOutput;
+}
+
+void DataSet::readFromFile() {
+
+	std::string line;
+
+	std::ifstream dataFile(this->path);
+
+	if (dataFile.is_open())
+	{
+		while (getline(dataFile, line))
+		{
+			this->dataSet.push_back(Sample(line, sizeOfInput, sizeOfOutput));
+		}
+		dataFile.close();
+	}
+	else printf("Unable to open file");
+}
+
