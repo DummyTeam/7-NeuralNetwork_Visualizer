2cd879f3c39a3482c5e115f3f2c4308fe75cd5a0
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-26 05:51:02 +0400

    Added a new cost function

--- Neural Network/NeuralNetwork.cpp
+++ Neural Network/NeuralNetwork.cpp
@@ -1,6 +1,6 @@
 #include"NeuralNetwork.h"
 #include <string>
-
+#include<iostream>
 NeuralNetwork::NeuralNetwork()
 {
 
@@ -28,12 +28,8 @@ std::vector<double> NeuralNetwork::predict(std::vector<double> &inputValues)
 
 	this->layers.at(0)->assignValues(inputValues);
 
-	for (std::vector<Layer*>::iterator layerIt = this->layers.begin(); layerIt != this->layers.end(); ++layerIt)
-	{
-		for (std::vector<Neuron*>::iterator neuronIt = (*layerIt)->getNeurons().begin(); neuronIt != (*layerIt)->getNeurons().end(); ++neuronIt)
-		{
-			(*neuronIt)->calculateActivation();
-		}
+	for (std::vector<Layer*>::iterator layerIt = this->layers.begin(); layerIt != this->layers.end(); ++layerIt) {
+		(*layerIt)->calculateActivations();
 	}
 
 	if (this->layers.size() > 1) {
@@ -47,7 +43,6 @@ void NeuralNetwork::addLayer(Layer* layer) {
 	this->layers.push_back(layer);
 }
 
-
 std::string NeuralNetwork::toString() {
 	std::string view = "";
 
@@ -59,47 +54,58 @@ std::string NeuralNetwork::toString() {
 		}
 		view += "\n-----------------------------------\n";
 	}
+
+	view += "\n";
 	return view;
 }
 
-void NeuralNetwork::train(DataSet* dataSet) {
+void NeuralNetwork::train(DataSet* dataSet, double learningRate) {
 	for (std::vector<Sample*>::iterator trainingSample = dataSet->getDataSet().begin(); trainingSample != dataSet->getDataSet().end(); trainingSample++)
 	{
 		this->backPropogate(
-			predict((*trainingSample)->getInput())	// Output layer activation values after feedforward propogation
-			, (*trainingSample)->getOutput()		// Expected results
+			predict((*trainingSample)->getInput()),	// Output layer activation values after feedforward propogation
+			(*trainingSample)->getOutput(),			// Expected results
+			learningRate							// Learning rate
 		);
+
+		printf("Cost: %f\n", costFunction((*trainingSample)->getOutput()));
+		std::cout << this->toString() << std::endl;
 	}
 }
 
-void NeuralNetwork::backPropogate(std::vector<double> const &results, std::vector<double> const &expected) {
+void NeuralNetwork::backPropogate(std::vector<double> const &results, std::vector<double> const &expected, double learningRate) {
 	if (results.size() != expected.size() || results.size() != (this->layers.at(this->layers.size() - 1)->getNeurons()).size())
 		throw "Size of the resulting array of numbers from prediction does not match with the size of expected value array. Number of expected values should be equal to the number of output layer neurons.";
 
 	for (std::vector<Layer*>::reverse_iterator layer = this->layers.rbegin(); layer != this->layers.rend(); layer++) {
-		(*layer)->calculateDelta();
+		(*layer)->calculateDelta(expected, layer == this->layers.rbegin());
 	}
 
 	for (std::vector<Layer*>::iterator layer = this->layers.begin(); layer != this->layers.end(); layer++) {
 		if (layer != this->layers.begin())
-			(*layer)->updateWeightsAndBiases();
+			(*layer)->updateWeightsAndBiases(learningRate);
 	}
+}
+
+double NeuralNetwork::costFunction(std::vector<double> const & expectedValues) {
+	std::vector<double> values = this->layers.at(this->layers.size() - 1)->getListActivationValues();
+
+	if (expectedValues.size() != values.size())
+		throw "Cannot calculate the cost. Number of expected outputs does not match with number of output layer neurons";
+
+	double cost = 0;
+
+	for (size_t i = 0; i < expectedValues.size(); i++) {
+		cost += pow(values[0] - expectedValues[0], 2);
+	}
+
+	return cost;
 }
 
 void NeuralNetwork::buildWeightsAndBiases() {
 	for (std::vector<Layer*>::iterator layer = this->layers.begin(); layer != this->layers.end(); layer++) {
 		if (layer != this->layers.begin()) {
-			for (std::vector<Neuron*>::iterator neuron = (*layer)->getNeurons().begin(); neuron != (*layer)->getNeurons().end(); neuron++) {
-				(*neuron)->initWeightsAndBias((*(layer - 1))->getNeurons());
-			}
+			(*layer)->buildWeightsAndBiases(*(layer - 1));
 		}
 	}
 }
-
-//void NeuralNetwork::setDataSet(DataSet* dataSet) {
-//	this->dataSet = dataSet;
-//}
-//
-//const DataSet& NeuralNetwork::getDataSet() {
-//	return *dataSet;
-//}
--- Neural Network/NeuralNetwork.h
+++ Neural Network/NeuralNetwork.h
@@ -20,7 +20,7 @@ public:
 
 	// Calculates delta values for all neurons and updates all the weights and biases
 	// based on that delta values
-	void backPropogate(std::vector<double> const &, std::vector<double> const &);
+	void backPropogate(std::vector<double> const &, std::vector<double> const &, double);
 
 	// Takes vector of values and assigns it to input layer neurons.
 	// Then calculates activations throughout the network.
@@ -29,13 +29,16 @@ public:
 	// Takes a DataSet and runs each sample through predict function.
 	// Then takes the output layer result and pass it through backPropogation function
 	// with expected values in the sample.
-	void train(DataSet*);
+	void train(DataSet*, double);
 
 	// Returns a string which represent neurons in each layer with activation values 
 	std::string toString();
 
 	// Creates new Weight objects and bias values for each neuron
 	void buildWeightsAndBiases();
+
+	// Last layer activations subtracted from expected value
+	double costFunction(std::vector<double> const &);
 
 	//void setDataSet(DataSet*);
 	//const DataSet& getDataSet();
