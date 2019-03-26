344d8030fc9c2e5b78274898b3aac6e78fd5ba51
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-26 19:16:48 +0400

    Removed unnecessary layers and test lines

--- Neural Network/Main.cpp
+++ Neural Network/Main.cpp
@@ -3,14 +3,16 @@
 #include"Utils.h"
 #include"DataSet.h"
 #include"ReLU.h"
+#include"Sigmoid.h"
 
 int main() {
+
 	// Initialize random seed
 	srand(10);
 
 	DataSet* dataSet = new DataSet("data.data", 2, 1);
-	double learningRate = 0.5;
-	system("pause");
+
+	double learningRate = 0.2;
 
 	// Create a neural network
 	NeuralNetwork* neuralNetwork = new NeuralNetwork();
@@ -19,28 +21,28 @@ int main() {
 	Layer* inputLayer = new Layer();
 
 	// Add created neurons to the created input layer
-	inputLayer->populateNeurons(2);
+	inputLayer->populateNeurons(2, neuralNetwork);
 
 	// Add input layer to neural network
 	neuralNetwork->addLayer(inputLayer);
 
 	// Create hidden layer
-	Layer* hiddenLayer = new Layer();
+	Layer* hiddenLayer = new Layer(new Sigmoid());
 	//Layer* hiddenLayer2 = new Layer();
 
 	// Add created neurons to hidden layer
-	hiddenLayer->populateNeurons(3);
-	//hiddenLayer2->populateNeurons(4);
+	hiddenLayer->populateNeurons(2, neuralNetwork);
+	//hiddenLayer2->populateNeurons(3, neuralNetwork);
 
 	// Add hidden layer to neural network
 	neuralNetwork->addLayer(hiddenLayer);
 	//neuralNetwork->addLayer(hiddenLayer2);
 
 	// Create an output layer
-	Layer* outputLayer = new Layer();
+	Layer* outputLayer = new Layer(new ReLU());
 
 	// Add created neurons to output layer
-	outputLayer->populateNeurons(1);
+	outputLayer->populateNeurons(1, neuralNetwork);
 
 	// Add output layer to neural network
 	neuralNetwork->addLayer(outputLayer);
@@ -50,7 +52,6 @@ int main() {
 
 	// Train the network
 	neuralNetwork->train(dataSet, learningRate);
-
 
 	std::cout << neuralNetwork->toString() << std::endl;
 
