1df27be5212cc13ef6e050c1894dc3a4a38ce823
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-03-30 00:54:56 +0400

    Added two different alternative implementation methods to Main

--- Neural Network/Main.cpp
+++ Neural Network/Main.cpp
@@ -1,14 +1,38 @@
 #include "Core.h"
 #include "Xor.h"
 
-int main() {
+int main()
+{
 	// Initialize random seed
 	srand(24);
 
-	// Neural Network for XOR 
+	// Alternative 1: Neural Network for XOR
 	Xor* x = new Xor("data.data", 0.1, 52000);
 	x->train();
 	x->viewNetwork();
+
+	printf("\n\n\n");
+	system("pause");
+
+	// Alternative 2: Neural Network for XOR
+	int numberOfInputs = 2;
+	int numberOfHiddenLayerNodes = 2;
+	int numberOfOutputs = 1;
+
+	NeuralNetwork* nn = (new NeuralNetwork::Builder())
+		->addLayer(new Layer(numberOfInputs))
+		->addLayer(new Layer(numberOfHiddenLayerNodes))
+		->addLayer(new Layer(numberOfOutputs))
+		->build();
+
+	double learningRate = 0.1;
+	int numberOfIterations = 20000;
+
+	nn->train(
+		new DataSet("data.data", numberOfInputs, numberOfOutputs), 
+		learningRate, 
+		numberOfIterations
+	);
 
 	system("pause");
 	return 0;
