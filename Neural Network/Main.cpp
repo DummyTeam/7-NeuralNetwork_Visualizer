6db8e887405fc6c075d0ef9c58124076aa4ebd17
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-02 00:14:18 +0400

    Added SFML graphics library for NN visualization

--- Neural Network/Main.cpp
+++ Neural Network/Main.cpp
@@ -1,45 +1,62 @@
 #include "Core.h"
 #include "Xor.h"
 #include "ReLU.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	// Initialize random seed
-	srand(24);
 
-	// Alternative 1: Neural Network for XOR
-	//Xor* x = new Xor("data.data", 0.1, 52000);
-	//x->train();
-	//x->viewNetwork();
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Neural Network Visualization");
 
-	printf("\n");
-	system("pause");
+	while (window.isOpen())
+	{
+		// handle events
 
-	// Alternative 2: Neural Network for XOR
-	int numberOfInputs = 22;
-	int numberOfHiddenLayerNodes = 16;
-	int numberOfHiddenLayerNodes2 = 8;
-	int numberOfOutputs = 1;
+		// update game logic
 
-	NeuralNetwork* nn = (new NeuralNetwork::Builder())
-		->addLayer(new Layer(numberOfInputs))
-		->addLayer(new Layer(numberOfHiddenLayerNodes))
-		->addLayer(new Layer(numberOfHiddenLayerNodes2))
-		->addLayer(new Layer(numberOfOutputs, new ReLU()))
-		->build();
+		window.clear();
 
-	double learningRate = 0.06;
-	int numberOfIterations = 200000;
+		// window.draw()
 
-	nn->train(
-		new DataSet("newDataSet.data", numberOfInputs, numberOfOutputs),
-		learningRate,
-		numberOfIterations
-	);
+		window.display();
+	}
 
-	system("pause");
+	//// Initialize random seed
+	//srand(24);
 
-	nn->test(new DataSet("newDataSetTest.data", numberOfInputs, numberOfOutputs));
+	//// Alternative 1: Neural Network for XOR
+	////Xor* x = new Xor("data.data", 0.1, 52000);
+	////x->train();
+	////x->viewNetwork();
+
+	//printf("\n");
+	//system("pause");
+
+	//// Alternative 2: Neural Network for XOR
+	//int numberOfInputs = 22;
+	//int numberOfHiddenLayerNodes = 16;
+	//int numberOfHiddenLayerNodes2 = 8;
+	//int numberOfOutputs = 1;
+
+	//NeuralNetwork* nn = (new NeuralNetwork::Builder())
+	//	->addLayer(new Layer(numberOfInputs))
+	//	->addLayer(new Layer(numberOfHiddenLayerNodes))
+	//	->addLayer(new Layer(numberOfHiddenLayerNodes2))
+	//	->addLayer(new Layer(numberOfOutputs, new ReLU()))
+	//	->build();
+
+	//double learningRate = 0.06;
+	//int numberOfIterations = 200000;
+
+	//nn->train(
+	//	new DataSet("newDataSet.data", numberOfInputs, numberOfOutputs),
+	//	learningRate,
+	//	numberOfIterations
+	//);
+
+	//system("pause");
+
+	//nn->test(new DataSet("newDataSetTest.data", numberOfInputs, numberOfOutputs));
 
 	system("pause");
 
