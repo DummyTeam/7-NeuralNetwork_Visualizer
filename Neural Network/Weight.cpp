74fbc2b5d3f67755274be697694c588557f390c7
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-06 00:03:15 +0400

    Added draw, getValueColor functions with updated constructor

--- Neural Network/Weight.cpp
+++ Neural Network/Weight.cpp
@@ -1,14 +1,9 @@
 #include "Weight.h"
 #include "Utils.h"
+#include "Neuron.h"
 
-Weight::Weight()
-{
+Weight::Weight() {
 	this->setValue(Utils::randomNumber());
-}
-
-Weight::~Weight()
-{
-	// Not implemented
 }
 
 Weight::Weight(double value) {
@@ -19,11 +14,42 @@ Weight::Weight(double value, Neuron* from, Neuron* to) {
 	this->setValue(value);
 	this->from = from;
 	this->to = to;
+
+	this->weightLine[0] = sf::Vertex(from->getCenterPoint());
+	this->weightLine[0].color = sf::Color(12, 12, 12, 255);
+
+	this->weightLine[1] = sf::Vertex(to->getCenterPoint());
+	this->weightLine[1].color = sf::Color(12, 12, 12, 255);
+}
+
+void Weight::init() {
+
 }
 
 void Weight::setValue(double value) {
+
+	this->getValueColor();
 	this->value = value;
 }
+
+void Weight::getValueColor() {
+	int r = 255;
+	int g = 255;
+
+	// Max weight limit is 2 (arbitrary)
+	r = 127 - 127 * this->value / 2;
+	g = 127 + 127 * this->value / 2;
+
+	r = (r > 255) ? 255 : r;
+	g = (g > 255) ? 255 : g;
+	r = (r < 0) ? 0 : r;
+	g = (g < 0) ? 0 : g;
+
+	sf::Color color = sf::Color(r, g, 0, 255);
+	this->weightLine[0].color = color;
+	this->weightLine[1].color = color;
+}
+
 
 double Weight::getValue() {
 	return this->value;
@@ -37,7 +63,6 @@ Neuron* Weight::getDestinationNeuron() {
 	return to;
 }
 
-void Weight::draw(sf::RenderWindow& window)
-{
-	window.draw(*(this->sprite));
+void Weight::draw(sf::RenderWindow* window) {
+	window->draw(this->weightLine, 2, sf::Lines);
 }
--- Neural Network/Weight.h
+++ Neural Network/Weight.h
@@ -1,17 +1,17 @@
 #pragma once
-
-#include "Drawable.h"
+#include "Visualizable.h"
 
 class Neuron;
 
-class Weight : public Drawable
+class Weight : public Visualizable
 {
 public:
 	Weight();
-	~Weight();
-
 	Weight(double value);
 	Weight(double value, Neuron* from, Neuron* to);
+
+	// Mutual init function
+	void init();
 
 	void setValue(double value);
 	double getValue();
@@ -20,11 +20,17 @@ public:
 
 	Neuron* getDestinationNeuron();
 
-	void draw(sf::RenderWindow&);
+	void draw(sf::RenderWindow*);
+
 
 private:
 	double value;
 	Neuron* from;		// from which neuron (id)
 	Neuron* to;			// to which neuron (id)
-	sf::Sprite* sprite;
+
+	sf::Sprite* shape;
+	sf::Vertex weightLine[2];
+
+	void getValueColor();
+
 };
