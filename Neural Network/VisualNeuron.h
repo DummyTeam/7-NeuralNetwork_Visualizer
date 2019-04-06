ad260f2503ae4caf7aa00e35f317597d28a02e0f
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-06 21:05:19 +0400

    Created VisualNeural.h class

---
+++ Neural Network/VisualNeuron.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+#include "VisualWeight.h"
+
+class VisualNeuron : public VisualObject
+{
+public:
+	VisualNeuron(Neuron*);
+	
+	sf::Text* getText();
+	sf::CircleShape* getShape();
+	void draw(sf::RenderWindow*);
+	sf::Vector2f getCenterPoint();
+	const sf::Vector2f& getInitPoint();
+
+private:
+	Neuron* neuronPointer;
+
+	std::vector<VisualWeight*> incomingVisualWeights;
+	std::vector<VisualWeight*> outgoingVisualWeights;
+
+	sf::CircleShape* shape;
+	sf::Text* text;
+	sf::Font* font;
+
+	float shapeWidth;
+	float shapeHeight;
+};
