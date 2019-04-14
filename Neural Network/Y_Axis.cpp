50e6ff3718514e7578ca152c492093c90925bf46
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-14 13:40:04 +0400

    Updated Y_Axis.cpp

--- Neural Network/Y_Axis.cpp
+++ Neural Network/Y_Axis.cpp
@@ -3,5 +3,11 @@
 
 Y_Axis::Y_Axis()
 {
+	this->height = 200.f;
+	this->width = 3.f;
+
+	this->axis = new sf::RectangleShape(sf::Vector2f(this->width, this->height));
+	this->axis->setFillColor(sf::Color::Yellow);
+	this->axis->setPosition(sf::Vector2f(20.0f, 20.0f));
 
 }
