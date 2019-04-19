ec4f981323539455fe1d5eccf8368266820acdbc
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-20 00:09:51 +0400

    Created LearningMethod.h

---
+++ Neural Network/LearningMethod.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "DataSet.h"
+#include "CostFunction.h"
+
+class NeuralNetwork;
+
+class LearningMethod
+{
+public:
+	virtual void train(DataSet*, double, int) = 0;
+	virtual void setNeuralNetwork(NeuralNetwork*) = 0;
+	virtual double calculateCost(std::vector<double> const &) = 0;
+	virtual const std::vector<double> & getCostHistory() = 0;
+
+protected:
+	CostFunction* cost;
+	NeuralNetwork* nn;
+	std::vector<double> costHistory;
+};
