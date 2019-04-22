5cc30d5d7138b0973c399488827e7c3ace4bbeb3
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-22 15:46:45 +0400

    Updated Y_Axis class

--- Neural Network/Y_Axis.cpp
+++ Neural Network/Y_Axis.cpp
@@ -2,7 +2,19 @@
 
 Y_Axis::Y_Axis()
 {
-	this->axis = new sf::RectangleShape(sf::Vector2f(this->width, this->height));
-	this->axis->setFillColor(sf::Color::Yellow);
+	this->axis->setSize(sf::Vector2f(this->width, this->height));
+	this->axis->setFillColor(sf::Color(166, 1, 180));
 	this->axis->setPosition(sf::Vector2f(20.0f, 20.0f));
+}
+
+void Y_Axis::setPosition(sf::Vector2f pos) {
+	this->axis->setPosition(pos);
+
+	for (size_t i = 0; i < this->axisNumbers.size(); i++)
+	{
+		this->axisNumbers.at(i)->setPosition(
+			sf::Vector2f(this->axis->getPosition().x - 30,
+				this->axis->getPosition().y + (this->height * i) / this->axisNumbers.size())
+		);
+	}
 }
--- Neural Network/Y_Axis.h
+++ Neural Network/Y_Axis.h
@@ -5,6 +5,7 @@ class Y_Axis : public Axis
 {
 public:
 	Y_Axis();
+	void setPosition(sf::Vector2f pos);
 
 private:
 
