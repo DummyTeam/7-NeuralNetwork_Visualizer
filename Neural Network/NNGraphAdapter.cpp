0a87cf553e0b831e163e7427a7e3e7ce34193f6e
Amiraslan Bakhshili <aslanbaxisli.ba@gmail.com>
2019-04-22 15:28:46 +0400

    Added new methods to NNGraphAdapter

--- Neural Network/NNGraphAdapter.cpp
+++ Neural Network/NNGraphAdapter.cpp
@@ -1,14 +1,28 @@
 #include "NNGraphAdapter.h"
+#include "GraphRangeStyle.h"
 
-
-NNGraphAdapter::NNGraphAdapter(NeuralNetwork* nn)
+NNGraphAdapter::NNGraphAdapter(NeuralNetwork* nn, GraphRangeStyle* graphRangeStyle)
 {
 	this->nn = nn;
-	this->graph = new Graph(nn->getLearningMethod()->getCostHistory());
+	this->graph = new Graph(nn->getLearningMethod()->getCostHistory(), graphRangeStyle);
+	this->screenMessage = new ScreenMessage();
 }
 
-void NNGraphAdapter::draw(sf::RenderWindow* window) 
-{
-	this->graph->draw(window);
+void NNGraphAdapter::init(sf::RenderWindow* renderWindow) {
+	this->renderWindow = renderWindow;
+
+	int width = (renderWindow->getSize().x - this->graph->getSize().x) / 2.0;
+	int height = (renderWindow->getSize().y - this->graph->getSize().y) / 2.0;
+
+	this->graph->setPosition(sf::Vector2f(width, height));
 }
 
+void NNGraphAdapter::draw() {
+	this->graph->draw(this->renderWindow);
+
+	this->screenMessage->setMessage(
+		"Stage: " + this->nn->getCurrentStage() + "\n" +
+		"Cost: " + std::to_string(this->nn->getLearningMethod()->getCurrentCost())
+	);
+	this->screenMessage->draw(this->renderWindow);
+}
--- Neural Network/NNGraphAdapter.h
+++ Neural Network/NNGraphAdapter.h
@@ -1,16 +1,22 @@
 #pragma once
 #include "Visualizable.h"
 #include "NeuralNetwork.h"
+#include "ScreenMessage.h"
 #include "Graph.h"
 
 
 class NNGraphAdapter : public Visualizable
 {
 public:
-	NNGraphAdapter(NeuralNetwork*);
-	void draw(sf::RenderWindow*);
+	NNGraphAdapter(NeuralNetwork*, GraphRangeStyle*);
+	void init(sf::RenderWindow*);
+	void draw();
 
 private:
 	Graph* graph;
 	NeuralNetwork* nn;
+	ScreenMessage* screenMessage;
+
+	sf::RenderWindow* renderWindow;
+
 };
